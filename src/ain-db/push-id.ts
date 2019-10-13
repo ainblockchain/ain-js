4baffa4eac52b6b78e3544ff87034c960bed44ce
liayoo <lia@comcom.ai>
2019-10-13 20:33:10 +0900

    Remove empty lines.

--- src/ain-db/push-id.ts
+++ src/ain-db/push-id.ts
@@ -25,11 +25,9 @@ export const PushId: IUID = {
    * Generates uid push identifier.
    */
   generate: (function () {
-
     // Timestamp of last push, used to prevent local
     // collisions if you push twice in one ms.
     let lastPushTime = 0;
-
     // We generate 72-bits of randomness which get
     // turned into 12 characters and appended to the
     // timestamp to prevent collisions with other clients.
@@ -37,30 +35,25 @@ export const PushId: IUID = {
     // in the event of a collision, we'll use those same
     // characters except "incremented" by one.
     const lastRandChars: any[] = [];
-
     return function () {
       let now = new Date().getTime();
       let duplicateTime = (now === lastPushTime);
       lastPushTime = now;
       let timeStampChars: string[] = new Array(8);
       let i: number;
-
       for (i = 7; i >= 0; i--) {
         timeStampChars[i] = ASCII_CHARS.charAt(now % 64);
         // NOTE: Can't use << here because javascript
         // will convert to int and lose the upper bits.
         now = Math.floor(now / 64);
       }
-
       if (now !== 0) { throw new Error('We should have converted the entire timestamp.'); }
       let id = timeStampChars.join('');
-
       if (!duplicateTime) {
         for (i = 0; i < 12; i++) {
           lastRandChars[i] = Math.floor(Math.random() * 64);
         }
       }
-
       // If the timestamp hasn't changed since
       // last push, use the same random number,
       // except incremented by 1.
@@ -70,17 +63,14 @@ export const PushId: IUID = {
         }
         lastRandChars[i]++;
       }
-
       for (i = 0; i < 12; i++) {
         id += ASCII_CHARS.charAt(lastRandChars[i]);
       }
-
       if (id.length !== 20) { throw new Error('Length should be 20.'); }
       return id;
     };
-
   })(),
-
+  
   /**
    * Timestamp
    * Gets timestamp from provided uid.
@@ -89,15 +79,11 @@ export const PushId: IUID = {
     return function (id: string, asDate?: boolean): number | Date {
       let time = 0;
       const data = id.substr(0, 8);
-
       for (let i = 0; i < 8; i++) {
         time = time * 64 + ASCII_CHARS.indexOf(data[i]);
       }
-
       if (asDate) { return new Date(time); }
       return time;
     };
-
   })()
-
 };
