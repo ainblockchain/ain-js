60be7eec3a9bcd371d57a6b5e317952d89ee66e9
Dongil Seo <seo@comcom.ai>
2022-05-06 14:38:59 +0900

    Add test_data.ts for testing

--- __tests__/test_data.ts
+++ __tests__/test_data.ts
@@ -1,11 +1,8 @@
-export const test_keystore  = '';
+export const test_keystore  = '{"version":3,"id":"253e91a5-3246-4330-b7be-ea1a77b179e1","address":"09a0d53fdf1c36a131938eb379b98910e55eefe1","crypto":{"ciphertext":"6c04b55a8562b60575f1619e83cdffa44d6a642c15e4f21491a3e413f806bb41","cipherparams":{"iv":"2c1637d89bf674c8b87e54120b7f9af3"},"cipher":"aes-128-ctr","kdf":"scrypt","kdfparams":{"dklen":32,"salt":"5ef6abd39bbeae66472ae77629ad17ffa0c0647dcc84837495cccc4efddc495d","n":262144,"r":8,"p":1},"mac":"9bd0e43d4db4871a882b8376094aabdf041328c307b4549f18f0d2f55dcb2d42"}}';
+export const test_pw = 'ainetwork';
+export const test_seed = 'receive ocean impact unaware march just dragon easy power muscle together flip';
+export const test_sk = 'ee0b1315d446e5318eb6eb4e9d071cd12ef42d2956d546f9acbdc3b75c469640';
 
-export const test_pw = '';
-
-export const test_seed = '';
-
-export const test_sk = '';
-
-export const test_node_1 = '';
-export const test_node_2 = '';
-export const test_event_handler_node = '';
+export const test_node_1 = 'http://localhost:8081';
+export const test_node_2 = 'http://localhost:8081';
+export const test_event_handler_node = 'http://localhost:8083';
