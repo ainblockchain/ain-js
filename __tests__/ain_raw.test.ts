3f4b5a845f653c1e818b3f197233c53d98887e2a
ehgmsdk20 <kdh75576795@gmail.com>
2022-07-26 19:05:18 +0900

    Fix broken tests

--- __tests__/ain_raw.test.ts
+++ __tests__/ain_raw.test.ts
@@ -364,12 +364,13 @@ describe('ain-js', function() {
     });
 
     it('get with empty op_list', async function() {
+      const protoVer = (await ain.net.getProtocolVersion()).result;
       expect(await ain.db.ref(allowed_path).get([]))
       .toEqual({
         "result": null,
         "code": 30006,
         "message": "Invalid op_list given",
-        "protoVer": "1.0.7",
+        "protoVer": protoVer,
       });
     });
   });
