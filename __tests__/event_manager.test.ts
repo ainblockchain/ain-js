2ff8fe61be238530cc1bc6b66dde29c1ad3d957d
ehgmsdk20 <kdh75576795@gmail.com>
2022-07-26 17:51:52 +0900

    Add tx_state_change event

--- __tests__/event_manager.test.ts
+++ __tests__/event_manager.test.ts
@@ -1,4 +1,5 @@
 import Ain from '../src/ain';
+import { FilterDeletionReasons } from '../src/types';
 
 const { test_event_handler_node } = require('./test_data');
 const delayMs = (time) => new Promise(resolve => setTimeout(resolve, time));
@@ -108,6 +109,67 @@ describe('Event Handler', function() {
       ain.em.subscribe('VALUE_CHANGED', {
         path: '/..wrong_app_name',
         event_source: null,
+      }, (event) => {
+      }, (err) => {
+        done();
+      });
+    });
+  });
+
+  describe('TX_STATE_CHANGED', () => {
+    const [ testAccount ] = ain.wallet.create(1);
+    const testAppName = `test_2${Date.now()}`;
+    const testAppPath = `/apps/${testAppName}`;
+    const mockConsoleLog = jest.spyOn(console, 'log');
+
+    beforeAll(async () => {
+      // Create test app
+      ain.wallet.setDefaultAccount(testAccount);
+      await ain.db.ref(`/manage_app/${testAppName}/create/${Date.now()}`).setValue({
+        value: { admin: { [testAccount]: true } },
+      });
+    });
+
+    afterAll(() => {
+      mockConsoleLog.mockRestore();
+    })
+
+    it('Subscribe to TX_STATE_CHANGED and deleted because end state reached', (done) => {
+      let filterId;
+      ain.db.ref(testAppPath).setValue({
+        value: Date.now(),
+      }).then((result)=>{
+        filterId = ain.em.subscribe('TX_STATE_CHANGED', {
+          tx_hash: result.tx_hash,
+          timeout: 60000,
+        }, (event) => {
+          expect(event.tx_state.before).toBe('EXECUTED');
+          expect(event.tx_state.after).toBe('FINALIZED');
+          setTimeout(() => {
+            expect((console.log as jest.Mock).mock.calls.length).toBe(1);
+            expect((console.log as jest.Mock).mock.calls[0][0]).toBe(`Event filter(id: ${filterId}) is deleted because of ${FilterDeletionReasons.END_STATE_REACHED}`);
+            done();
+          }, 5000);
+        })
+      });
+    }, 70000);
+
+    it('Subscribe to TX_STATE_CHANGED and deleted because of timeout', (done) => {
+      const filterId = ain.em.subscribe('TX_STATE_CHANGED', {
+        tx_hash: '0x9ac44b45853c2244715528f89072a337540c909c36bab4c9ed2fd7b7dbab47b2',
+        timeout: 60000,
+      })
+      setTimeout(() => {
+        expect((console.log as jest.Mock).mock.calls.length).toBe(1);
+        expect((console.log as jest.Mock).mock.calls[0][0]).toBe(`Event filter(id: ${filterId}) is deleted because of ${FilterDeletionReasons.FILTER_TIMEOUT}`);
+        done();
+      }, 61000);
+    }, 70000);
+
+    it('Subscribe to TX_STATE_CHANGED with wrong config', (done) => {
+      ain.em.subscribe('TX_STATE_CHANGED', {
+        tx_hash: '123',
+        timeout: 0,
       }, (event) => {
       }, (err) => {
         done();
