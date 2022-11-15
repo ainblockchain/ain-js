4a734b7b7c6bc5a88d8c7be54ec8f5ac9931e957
ehgmsdk20 <kdh75576795@gmail.com>
2022-11-15 18:43:42 +0900

    Change relative path to absolute path

--- test-pipeline.sh
+++ test-pipeline.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -l
 
-cd ain-blockchain
+cd /app/ain-blockchain
 if [[ "$1" = 'dev']]; then
   git checkout develop
 fi
 yarn install
 bash ./start_local_blockchain.sh
 sleep 10s
-cd ../ain-js
+cd /app/ain-js
 yarn test
