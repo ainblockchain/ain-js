75ebbf0950bda7e995ae52b31a9e50d28f798f32
hyeonwoong <hyeonwoong@comcom.ai>
2023-08-16 16:22:57 +0900

    docs: add signer documentation using jsDoc.

--- src/signer.ts
+++ src/signer.ts
@@ -1,10 +1,28 @@
 import Wallet from "./wallet";
 
+/**
+ * Signer takes responsibility to sign message and transaction.
+ */
 export interface Signer {
+    /**
+     * Return checksum address to use signing message.
+     * @param {string} address
+     */
     getAddress(address?: string): string;
+
+    /**
+     * Sign message using signer's account.
+     * If user input specific address and signer has it, Message will be signed by the address.
+     * @param {any} message
+     * @param {string} address - The address registered to signer.
+     */
     signMessage(message: any, address?: string): Promise<string> | string;
 }
 
+/**
+ * DefaultSigner class takes responsibility to sign using the Wallet class.
+ * When ain class is initialized, DefaultSigner is set.
+ */
 export class DefaultSigner implements Signer {
     readonly wallet: Wallet;
 
