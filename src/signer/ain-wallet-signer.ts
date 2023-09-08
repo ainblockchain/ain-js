27e2a0da249bb9b3c3b8bf329838fcba11d64dd2
Dongil Seo <seo@comcom.ai>
2023-09-08 12:01:17 +0900

    Add more jsdoc comments for signer classes

--- src/signer/ain-wallet-signer.ts
+++ src/signer/ain-wallet-signer.ts
@@ -1,8 +1,15 @@
 import { Signer } from "./signer";
 
+/**
+ * A signer class for AIN Wallet chrome extension
+ * (https://chrome.google.com/webstore/detail/ain-wallet/hbdheoebpgogdkagfojahleegjfkhkpl).
+ */
 export class AinWalletSigner implements Signer {
   private ainetwork: Signer;
   
+  /**
+   * Initializes the class based on the AIN Wallet's global variable.
+   */
   constructor() {
     if (window.ainetwork) {
       this.ainetwork = window.ainetwork;
@@ -10,10 +17,22 @@ export class AinWalletSigner implements Signer {
       throw new Error("Not found ain wallet object");
     }
   }
+
+  /**
+   * Returns the checksum address to sign messages with.
+   * If the address is not given, the default address of the wallet is used.
+   * @param {string} address - The address of the account to sign the message with.
+   */
   getAddress(address?: string): string {
     return this.ainetwork.getAddress(address);
   }
 
+  /**
+   * Signs a message with the private key of the given address.
+   * If an address is not given, the default address of the wallet is used.
+   * @param {any} message - The message to sign.
+   * @param {string} address - The address of the account to sign the message with.
+   */
   signMessage(message: any, address?: string): Promise<string> | string {
     return this.ainetwork.signMessage(message, address);
   }
