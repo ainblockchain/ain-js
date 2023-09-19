36122d10bd9fefe1d7f2eaf4035b8d380d1f6088
Dongil Seo <seo@comcom.ai>
2023-09-19 15:58:20 +0900

    Add typedoc comments for Database class

--- src/ain-db/db.ts
+++ src/ain-db/db.ts
@@ -2,15 +2,19 @@ import Reference from './ref';
 import Ain from '../ain';
 import Provider from '../provider';
 
+/**
+ * A class for managing blockchain database states.
+ */
 export default class Database {
+  /** The network provider object. */
   public provider: Provider;
+  /** The Ain object. */
   private _ain: Ain;
 
   /**
-   * @param {Ain} ain
-   * @param {Provider} provider
-   *
-   * @constructor
+   * Creates a new Database object.
+   * @param {Ain} ain The Ain object.
+   * @param {Provider} provider The network provider object.
    */
   constructor(ain: Ain, provider: Provider) {
     this.provider = provider;
@@ -19,7 +23,7 @@ export default class Database {
 
   /**
    * Returns a reference instance of the given path.
-   * @param {String} path
+   * @param {String} path The path to refer to.
    * @return {Reference} A reference instance of the given path.
    */
   ref(path?: string): Reference {
