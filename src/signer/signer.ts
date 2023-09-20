c29f3338fa3475f0de51230a3776d9966e2dbce0
Dongil Seo <seo@comcom.ai>
2023-09-20 14:06:12 +0900

    Tweak typedoc comments for signers

--- src/signer/signer.ts
+++ src/signer/signer.ts
@@ -5,40 +5,43 @@ import { TransactionBody, TransactionInput } from "../types";
  */
 export interface Signer {
   /**
-   * Returns the checksum address to sign messages with.
-   * If the address is not given, the default address of the signer is used.
-   * @param {string} address - The address of the account to sign the message with.
+   * Gets an account's checksum address.
+   * If the address is not given, the default account of the signer is used.
+   * @param {string} address The address of the account.
+   * @returns {string} The checksum address.
    */
   getAddress(address?: string): string;
 
   /**
-   * Signs a message with the private key of the given address.
-   * If an address is not given, the default address of the signer is used.
-   * @param {string} message - The message to sign.
-   * @param {string} address - The address of the account to sign the message with.
+   * Signs a message using an account.
+   * If an address is not given, the default account of the signer is used.
+   * @param {string} message The message to sign.
+   * @param {string} address The address of the account.
+   * @returns {Promise<string> | string} The signature.
    */
   signMessage(message: string, address?: string): Promise<string> | string;
 
   /**
    * Signs and sends a transaction to the network.
-   * @param {TransactionInput} transactionObject
-   * @param {boolean} isDryrun - dryrun option.
-   * @returns {Promise<any>}
+   * @param {TransactionInput} transactionObject The transaction input object.
+   * @param {boolean} isDryrun The dryrun option.
+   * @returns {Promise<any>} The return value of the blockchain API.
    */
   sendTransaction(transactionObject: TransactionInput, isDryrun?: boolean);
 
   /**
-   * Sends signed transactions to the network.
-   * @param {TransactionInput[]} transactionObjects
+   * Signs and sends multiple transactions in a batch to the network.
+   * @param {TransactionInput[]} transactionObjects The list of the transaction input objects.
+   * @returns {Promise<any>} The return value of the blockchain API.
    */
   sendTransactionBatch(transactionObjects: TransactionInput[]): Promise<any>
 
   /**
-   * Sends a signed transaction to the network
-   * @param {string} signature
-   * @param {TransactionBody} txBody
-   * @param {boolean} isDryrun - dryrun option.
-   * @returns {Promise<any>}
+   * Sends a signed transaction to the network.
+   * @param {string} signature The signature.
+   * @param {TransactionBody} txBody The transaction body.
+   * @param {boolean} isDryrun The dryrun option.
+   * @returns {Promise<any>} The return value of the blockchain API.
    */
   sendSignedTransaction(signature: string, txBody: TransactionBody, isDryrun?: boolean): Promise<any>
 }
