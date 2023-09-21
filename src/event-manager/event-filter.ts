40750be970d48e838afaf908dd5a366d7265a57e
Dongil Seo <seo@comcom.ai>
2023-09-21 15:35:00 +0900

    Add typedoc comments to EventFilter class

--- src/event-manager/event-filter.ts
+++ src/event-manager/event-filter.ts
@@ -1,16 +1,32 @@
 import { BlockchainEventTypes, BlockchainEventConfig } from '../types';
 
+/**
+ * A class for filtering blockchain events.
+ */
 export default class EventFilter {
+  /** The event filter ID. */
   public readonly id: string;
+  /** The blockchain event type. */
   public readonly type: BlockchainEventTypes;
+  /** The blockchain event configuration. */
   public readonly config: BlockchainEventConfig;
 
+  /**
+   * Creates a new EventFilter object.
+   * @param {string} id The event filter ID object.
+   * @param {BlockchainEventTypes} type The blockchain event type value.
+   * @param {BlockchainEventConfig} config The blockchain event configuration object.
+   */
   constructor(id: string, type: BlockchainEventTypes, config: BlockchainEventConfig) {
     this.id = id;
     this.type = type;
     this.config = config;
   }
 
+  /**
+   * Converts to a javascript object.
+   * @returns {Object} The javascript object.
+   */
   toObject() {
     return {
       id: this.id,
