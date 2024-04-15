c5183365e0f7184bd1993b8eeda7299d44d48ee5
platfowner <seo@comcom.ai>
2024-04-15 13:44:18 +0900

    Add ping-pong message types

--- src/types.ts
+++ src/types.ts
@@ -350,6 +350,9 @@ export enum EventChannelMessageTypes {
   DEREGISTER_FILTER = 'DEREGISTER_FILTER',
   EMIT_EVENT = 'EMIT_EVENT',
   EMIT_ERROR = 'EMIT_ERROR',
+  // NOTE(platfowner): Message types for custom ping-pong (see https://github.com/ainblockchain/ain-js/issues/171).
+  PING = 'PING',
+  PONG = 'PONG',
 }
 
 /**
